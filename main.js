246e786296aa7a79975d1b56dcf413ac92378037
sweta <swetabisht4b@outlook.com>
2022-12-31 16:29:06 +0530

    initial commit

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
-const date=document.querySelector("#dateofbirth");
-const luckyNumber=document.querySelector("#luckynumber");
-const checkBtn=document.querySelector("#check");
-const outPut=document.querySelector("#output");
+const date = document.querySelector("#dateofbirth");
+const luckyNumber = document.querySelector("#luckynumber");
+const checkBtn = document.querySelector("#check");
+const outPut = document.querySelector("#output");
 
- checkBtn.addEventListener("click", checkBirthdayIsLucky);
+checkBtn.addEventListener("click", checkBirthDateIsLucky);
 
-function checkBirthdayIsLucky(){
-    let Date = date.value.replaceAll("-","")
-    let sum=0;
 
-   for(let char of Date){
-    sum += +char;
-   }
-      if (sum%luckyNumber.value==0){
-    outPut.innerText = "Your birthday is lucky.😃";
-}else{
-    outPut.innerText= "Your birthday is Not lucky.😔";
+function calSum() {
+    let Date = date.value.replaceAll("-", "")
+    let sum = 0;
+
+    for (let char of Date) {
+        sum += +char;
+    }
+    return sum;
 }
+
+
+
+function compareValues(sum,luckyNumber){
+    if(sum%luckyNumber===0){
+      outPut.innerText="Your birthday is lucky";
+    }else {
+      outPut.innerText="Your birthday is not lucky";
+    }
+  }
+  
+  function checkBirthDateIsLucky(){
+    const dob =date.value;
+    const sum = calSum(dob);
+    if(sum&&dob){
+    compareValues(sum,luckyNumber.value)
+    }else{ 
+    outPut.innerText="Please enter both the fields";
+  }
 }
+
